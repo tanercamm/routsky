76a99ba8b72bdaf2bf1c4ddb76d18f11bed0724c
Taner Çam <tanercamm@outlook.com>
2026-03-18 13:55:50 +0300

    add AvatarBase64 and ProfilePictureBase64 columns to Users and UserProfiles

---
+++ Routsky.Api/Migrations/20260318105510_FixAvatarColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Routsky.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAvatarColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarBase64",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureBase64",
+                table: "UserProfiles",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarBase64",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureBase64",
+                table: "UserProfiles");
+        }
+    }
+}
